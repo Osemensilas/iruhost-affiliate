d576fca52a013ebdb5f1741f7443a13238d3b238
unknown <osemensilas@gmail.com>
2026-09-12 17:15:55 +0100

    New Commit

---
+++ components/user/RegisterComponent.tsx
@@ -0,0 +1,252 @@
+'use client'
+
+import Image from "next/image";
+import { UserPlus, Users, CreditCard, ChartNoAxesCombined, User, Mail, Lock, Globe, Eye, EyeOff } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import Loading from "@/components/general/Loading";
+
+const RegisterComponent = () => {
+
+    const router = useRouter();
+
+    interface User {
+        firstname: string;
+        lastname: string;
+        email: string;
+        password: string;
+        confirmPassword: string;
+        country: string;
+    }
+
+    const [error, setError] = useState<string>("");
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [confirmVisible, setConfirmVisible] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [formData, setFormData] = useState<User>({
+        firstname: '',
+        lastname: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        country: '',
+    })
+
+    const showPassword = () => {
+        setPasswordVisible(true);
+    }
+
+    const hidePassword = () => {
+        setPasswordVisible(false);
+    }
+
+    const seeConfirm = () => {
+        setConfirmVisible(true);
+    }
+
+    const hideConfirm = () => {
+        setConfirmVisible(false);
+    }
+
+    const handleChanged = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = e.target;
+        
+        setFormData({...formData, [name]: value});
+    }
+
+    const formSubmitted = async () => {
+
+        console.log(formData);
+
+        setLoading(true);
+
+        const emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!formData.firstname || !formData.lastname || !formData.email || !formData.password || !formData.confirmPassword || !formData.country){
+            setError("All fields required");
+            setLoading(false);
+            return;
+        }
+
+        if (!emailValid.test(formData.email)){
+            setError("Invalid email address");
+            setLoading(false);
+            return;
+        }
+
+        if (formData.password !== formData.confirmPassword){
+            setError("Password do not match");
+            setLoading(false);
+            return;
+        }
+
+        setError("");
+
+        const url = "https://affiliate-backend.iruhost.com/api/register";
+
+        try{
+            const response = await axios.post(url, formData, {
+                headers: {
+                    "Content-Type" : "application/json"
+                },withCredentials: true
+            });
+
+            console.log(response.data);
+
+            if (response.data.status === "success"){
+
+                localStorage.setItem("token", response.data.token);
+                
+                router.push("/dashboard");
+            }
+            setLoading(false);
+        }catch(error){
+            if (axios.isAxiosError(error)){
+                console.log(error.response?.data.message);
+                setError(error.response?.data.message);
+                setLoading(false);
+            }
+        }
+    }
+
+    return ( 
+        <>
+        <section className="h-screen w-screen flex">
+            <div className="h-full w-1/2 hidden sm:flex flex-col justify-between items-center py-5">
+                <div className="relative h-10 w-15">
+                    <Image src={"/logo.png"} fill alt="logo" className="object-fill" />
+                </div>
+                <h2 className="text-4xl text-accent text-center font-bold">Join IruHost <br></br><span className="text-primary">Affiliate </span>Program</h2>
+                <p className="text-accent text-base text-center">Create your affliate account and start earning<br></br>commissions by referring IruHost services.</p>
+                <div className="relative h-62.5 w-100">
+                    <Image src="/computer-security-with-login-password-padlock.png" className="object-fill" fill alt="dashbord" />
+                </div>
+                <div className="h-max w-full flex px-20">
+                    <div className="h-max w-[30%] flex items-center justify-center flex-col gap-2">
+                        <div className="h-max w-max rounded-full p-5 bg-primary/20">
+                            <Users className="h-10 w-10 text-primary" />
+                        </div>
+                        <h2 className="text-base font-bold text-accent">Easy to Join</h2>
+                        <p className="text-sm text-grey text-center">Sign up for free and get your unique referral.</p>
+                    </div>
+                    <div className="h-max w-[30%] flex items-center justify-center flex-col gap-2">
+                        <div className="h-max w-max rounded-full p-5 bg-primary/20">
+                            <ChartNoAxesCombined className="h-10 w-10 text-primary" />
+                        </div>
+                        <h2 className="text-base font-bold text-accent">Earn More</h2>
+                        <p className="text-sm text-grey text-center">Earn attractive commissions for every successful referrals.</p>
+                    </div>
+                    <div className="h-max w-[30%] flex items-center justify-center flex-col gap-2">
+                        <div className="h-max w-max rounded-full p-5 bg-primary/20">
+                            <CreditCard className="h-10 w-10 text-primary" />
+                        </div>
+                        <h2 className="text-base font-bold text-accent">Timely Payouts</h2>
+                        <p className="text-sm text-grey text-center">We pay on time, everytime, no hassle.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="h-full w-full sm:w-1/2 bg-accent flex justify-center items-center">
+                <form onSubmit={(e) => e.preventDefault()} className="h-max w-full sm:w-4/5 shadow px-4 sm:px-10 py-10">
+                    <div className="h-max w-full flex justify-center mb-5">
+                        <UserPlus className="h-10 w-10 text-primary" />
+                    </div>
+                    <h2 className="text-center mb-5 font-bold text-3xl">Create Your <span className="text-primary">Account</span></h2>
+                    <p className="text-base text-center text-grey mb-5">Fill in the details below to get started.</p>
+                    <div className="h-max w-full flex flex-col gap-2">
+                        <div className={`w-full h-max py-2 text-accent bg-danger rounded text-center
+                            ${error ? "" : "hidden"}
+                            `}>{error}</div>
+                        <div className="h-max w-full flex items-center gap-2">
+                            <div className="h-max w-full relative">
+                                <label htmlFor="firstname" className="text-grey text-base">First Name:</label>
+                                <input type="text" id="firstname" name="firstname" value={formData.firstname} onChange={handleChanged} className="h-10 w-full border border-grey outline-none rounded px-10 text-sm" />
+                                <div className="absolute top-8 left-3 h-max w-max">
+                                    <User className="h-5 w-5 text-grey" />
+                                </div>
+                            </div>
+                            <div className="h-max w-full relative">
+                                <label htmlFor="lastname" className="text-grey text-base">Last Name:</label>
+                                <input type="text" id="lastname" name="lastname" value={formData.lastname} onChange={handleChanged} className="h-10 w-full border border-grey outline-none rounded px-10 text-sm" />
+                                <div className="absolute top-8 left-3 h-max w-max">
+                                    <User className="h-5 w-5 text-grey" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="h-max w-full relative">
+                            <label htmlFor="email" className="text-grey text-base">Email:</label>
+                            <input type="text" id="email" name="email" value={formData.email} onChange={handleChanged} className="h-10 w-full border border-grey outline-none rounded px-10 text-sm" />
+                            <div className="absolute top-8 left-3 h-max w-max">
+                                <Mail className="h-5 w-5 text-grey" />
+                            </div>
+                        </div>
+                        <div className="h-max w-full flex items-center gap-2">
+                            <div className="h-max w-full relative">
+                                <div className="h-max w-full flex items-center justify-between">
+                                    <label htmlFor="password" className="text-grey text-base">Password:</label>
+                                </div>
+                                <input type={passwordVisible ? "text" : "password"} id="password" name="password" value={formData.password} onChange={handleChanged} className="h-10 w-full border border-grey outline-none rounded px-10 text-sm" />
+                                <div className="absolute top-8 left-3 h-max w-max">
+                                    <Lock className="h-5 w-5 text-grey" />
+                                </div>
+                                <button type="button" onClick={showPassword} className={`absolute top-8 left-[85%] h-max w-max cursor-pointer
+                                    ${passwordVisible ? "hidden" : ""}
+                                    `}>
+                                    <Eye className="h-5 w-5 text-grey" />
+                                </button>
+                                <button type="button" onClick={hidePassword} className={`absolute top-8 left-[85%] h-max w-max cursor-pointer
+                                    ${passwordVisible ? "" : "hidden"}
+                                    `}>
+                                    <EyeOff className="h-5 w-5 text-grey" />
+                                </button>
+                            </div>
+                            <div className="h-max w-full relative">
+                                <div className="h-max w-full flex items-center justify-between">
+                                    <label htmlFor="confirmPassword" className="text-grey text-base">Confirm Password:</label>
+                                </div>
+                                <input type={confirmVisible ? "text" : "password"} id="confirmPassword" name="confirmPassword" value={formData.confirmPassword} onChange={handleChanged} className="h-10 w-full border border-grey outline-none rounded px-10 text-sm" />
+                                <div className="absolute top-8 left-3 h-max w-max">
+                                    <Lock className="h-5 w-5 text-grey" />
+                                </div>
+                                <button type="button" onClick={seeConfirm} className={`absolute top-8 left-[85%] h-max w-max cursor-pointer
+                                    ${confirmVisible ? "hidden" : ""}
+                                    `}>
+                                    <Eye className="h-5 w-5 text-grey" />
+                                </button>
+                                <button type="button" onClick={hideConfirm} className={`absolute top-8 left-[85%] h-max w-max cursor-pointer
+                                    ${confirmVisible ? "" : "hidden"}
+                                    `}>
+                                    <EyeOff className="h-5 w-5 text-grey" />
+                                </button>
+                            </div>
+                        </div>
+                        <div className="h-max w-full relative">
+                            <label htmlFor="country" className="text-grey text-base">Country:</label>
+                            <select name="country" id="country" onChange={handleChanged} className="h-10 w-full border border-grey outline-none rounded px-10 text-sm text-grey">
+                                <option value="">Select your country</option>
+                                <option value="nigeria">Nigeria</option>
+                            </select>
+                            <div className="absolute top-8 left-3 h-max w-max">
+                                <Globe className="h-5 w-5 text-grey" />
+                            </div>
+                        </div>
+                        <div className="h-max w-full flex items-center gap-2">
+                            <input type="checkbox" id="remember" className="cursor-pointer" />
+                            <label htmlFor="remember" className="text-base cursor-pointer text-grey">Remember Me</label>
+                        </div>
+                        <div className="h-max w-full">
+                            <button type="submit" onClick={formSubmitted} className="h-max w-full text-background rounded py-3 flex items-center justify-center text-center bg-primary text-base outline-none cursor-pointer">{loading ? <Loading /> : "Sign Up"}</button>
+                        </div>
+                        <div className="h-max w-full flex items-center gap-2 text-base text-grey">
+                            Already an account? <Link href={"/login"} className="text-primary">Sign In</Link>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </section>
+        </>
+     );
+}
+ 
+export default RegisterComponent;
